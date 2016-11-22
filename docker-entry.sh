06bb3a77367067388db7022d6e82cc662f27ab14
Leonardo Ludueña <elnomade@gmail.com>
2016-11-22 14:46:33 -0300

    Update docker-entry.sh

--- docker-entry.sh
+++ docker-entry.sh
@@ -10,6 +10,6 @@ until mysqladmin -hlocalhost -P3306 -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" processl
 	sleep 5
 done
 
-echo "Starting platform"
-cd mattermost
+echo "MatterMost: Starting platform"
+cd /opt/mattermost
 exec ./bin/platform -config=config/config_docker.json
